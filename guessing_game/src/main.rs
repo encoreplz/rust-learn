b18ddee1ace9a0783cc185e53f81b31469e641ab
encoreplz <evgheni.scelcov@gmail.com>
2020-07-24 14:27:45 +0300

    Change tabs to 2

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -3,32 +3,32 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-    let secret_number = rand::thread_rng().gen_range(1, 11);
+  let secret_number = rand::thread_rng().gen_range(1, 11);
 
-    loop {
-        println!("Please input your guess.");
+  loop {
+    println!("Please input your guess.");
 
-        let mut guess = String::new();
+    let mut guess = String::new();
 
-        stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
+    stdin()
+      .read_line(&mut guess)
+      .expect("Failed to read line");
 
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Enter a number!");
-                continue;
-            },
-        };
+    let guess: u32 = match guess.trim().parse() {
+      Ok(num) => num,
+      Err(_) => {
+        println!("Enter a number!");
+        continue;
+      },
+    };
 
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            },
-        }
+    match guess.cmp(&secret_number) {
+      Ordering::Less => println!("Too small!"),
+      Ordering::Greater => println!("Too big!"),
+      Ordering::Equal => {
+        println!("You win!");
+        break;
+      },
     }
+  }
 }
